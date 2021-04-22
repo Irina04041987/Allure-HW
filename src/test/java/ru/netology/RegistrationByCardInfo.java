155c383986f22786b14d76447878fd28fbec7239
Irina Platonova <Crazyangeli@yandex.ru>
2021-04-22 18:29:51 +0300

    add project

---
+++ src/test/java/ru/netology/RegistrationByCardInfo.java
@@ -0,0 +1,21 @@
+package ru.netology;
+
+import com.github.javafaker.PhoneNumber;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class RegistrationByCardInfo {
+    private final String city;
+    private  final String name;
+    private  final PhoneNumber phone;
+
+
+
+
+
+
+}
